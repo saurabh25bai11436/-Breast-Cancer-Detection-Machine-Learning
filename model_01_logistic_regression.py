6e8b9bf9c8028729635db3c7bd81ad27f54559bc
saurabh25bai11436 <saurabh.25bai11436@vitbhopal.ac.in>
2026-02-16 18:17:47 +0530

    Update model_01_logistic_regression.py

--- model_01_logistic_regression.py
+++ model_01_logistic_regression.py
@@ -1,6 +1,9 @@
+#Importing the Dependencies
 import numpy as np
 import pandas as pd
 import sklearn.datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+
+
