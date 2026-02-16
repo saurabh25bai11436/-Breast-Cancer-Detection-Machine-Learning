ea3a6d16d50d34c0c30ee68cea80f026076b5b81
saurabh25bai11436 <saurabh.25bai11436@vitbhopal.ac.in>
2026-02-16 15:54:29 +0530

    Add imports for data processing and model training

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_breast_cancer
