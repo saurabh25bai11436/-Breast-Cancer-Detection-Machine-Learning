4ea6964b6a9949ea8a8231f0c669c51c9cb5d533
saurabh25bai11436 <saurabh.25bai11436@vitbhopal.ac.in>
2026-02-16 15:56:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import load_breast_cancer
+import warnings
+warnings.filterwarnings('ignore')
+
+
